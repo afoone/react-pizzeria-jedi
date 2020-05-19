aa3c160ae63dd9a083d73e706c6fcc49366be0d3
jonadepr <jonadepr@gmail.com>
2020-05-19 12:31:43 +0200

    tabla lista de pizzas

--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchPizzas } from '../actions'
+import { Link } from 'react-router-dom'
 
 const PizzaList = props => {
 
     const { dispatch } = props;
-    console.log("propiedades en PizzaList",props)
+    console.log("propiedades en PizzaList", props)
     useEffect(
         () => {
             console.log("did mount");
@@ -16,9 +17,39 @@ const PizzaList = props => {
 
     return (
         <div className="pizza-list">
-            {props.pizzas.map(
-                item => <div> {item.id} {item.name}</div>
-            )}
+            <table className="ui striped table unstackable">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Novedad</th>
+                        <th>Precio</th>
+                        <th>Imagen</th>
+                        <th>Acciones</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        props.pizzas.map(
+                            e => <tr key={e.id}>
+                                <td>{e.id}</td>
+                                <td>{e.name}</td>
+                                <td>{e.novelty}</td>
+                                <td>{e.price}</td>
+                                <td><img src={e.image} alt="" border="3" height="100" width="100"></img></td>
+                                <td>
+                                    <a href={`https://www.w3schools.com`}>
+                                        <div>Ver</div>
+                                            </a>
+                                </td>
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+            </table>
         </div>
     )
 }
