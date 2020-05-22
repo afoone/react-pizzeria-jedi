b06320ab16d2dbe4063977b06287d1325e81d7de
jonadepr <jonadepr@gmail.com>
2020-05-22 18:50:19 +0200

    acceso solo a admin a borrar y editar pizzas

--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -3,7 +3,19 @@ import { connect } from 'react-redux'
 import { fetchPizzas } from '../actions'
 import { Link } from 'react-router-dom'
 import { db } from '../config/firebase'
+import { UsuarioContext } from "../context/UsuarioProvider";
+
+
+
+
 const PizzaList = props => {
+
+
+    const { usuario} = React.useContext(
+        UsuarioContext
+      );
+    console.log("usu", usuario.role)
+    
 
     const { dispatch } = props;
     //console.log("propiedades en PizzaList", props)
@@ -51,8 +63,9 @@ const PizzaList = props => {
                                 <td><img src={e.image} alt="" border="3" height="100" width="100"></img></td>
                                 <td>
                                     <Link to={`/pizzaId/${e.id}`}>vER</Link>{" "}
-                                    <Link to={`/pizzaedit/${e.id}`}>Editar</Link>{" "}
-                                    <a href="#lista" onClick={() => onBorrarClicked(e.id)}>Borrar</a>
+                                    {usuario.role==="admin"?
+                                    <><Link to={`/pizzaedit/${e.id}`}>Editar</Link>{" "}
+                                    <a href="#lista" onClick={() => onBorrarClicked(e.id)}>Borrar</a></>:""}
                                 </td>
                             </tr>
                         )
