2a5dd38bf91ba642db26d2005f19793e6d316ae0
cizquierdof <cizquierdof@gmail.com>
2020-05-22 21:02:51 +0200

    lista de comentarios

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,56 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment, useContext} from 'react'
 import { Form, Header, Input, Button, Segment } from 'semantic-ui-react'
+import { UsuarioContext } from '../context/UsuarioProvider'
 
 
 const CommentForm = (props) => {
 
+    const usuario = useContext(UsuarioContext);
+
+    //console.log("user detectado", usuario.usuario.displayName);
+
+
     const [score, setScore] = useState('');
     const [comment, setComment] = useState('')
-    
-    const onChangeValue=(e)=>{
-        if(e.target.name==='score'){
+
+
+
+    const onChangeValue = (e) => {
+        if (e.target.name === 'score') {
             setScore(e.target.value);
         }
-        else if(e.target.name==='comment'){
+        else if (e.target.name === 'comment') {
             setComment(e.target.value)
         }
     }
-     const onClickHandle=e=>{
-         const comm={
-             score:score,
-             comment:comment
-         }
 
-         props.commentHandle(comm)
 
-     }
+    const onClickHandle = e => {
+        const fecha = new Date().toLocaleDateString();
+        const comm = {
+            user: usuario.usuario.displayName,
+            date: fecha,
+            score: score,
+            comment: comment
+        }
+
+        props.commentHandle(comm)
+
+    }
 
     return (
-        <>
-        <Header as='h2'>Deja tu comentario</Header>
-        <Segment raised>
-        <Form>
-        <Form.Input
-            name='score'
-            control={Input}
-            type='number'
-            min={0}
-            max={5}
-            placeholder='Puntuación'
-            value={score}
-            onChange={onChangeValue}
-            />
-            <Form.TextArea
-            name='comment'
-            type='text'
-            placeholder='Deja aquí tu comentario'
-            value={comment}
-            onChange={onChangeValue}
-            />
-        </Form>
-        </Segment>
-        <Button content='Guardar' onClick={onClickHandle}/> 
-        </>   
+        <Fragment>
+            <Header as='h2'>Deja tu comentario</Header>
+            <Segment raised>
+                <Form>
+                    <Form.Input
+                        name='score'
+                        control={Input}
+                        type='number'
+                        min={0}
+                        max={5}
+                        placeholder='Puntuación'
+                        value={score}
+                        onChange={onChangeValue}
+                    />
+                    <Form.TextArea
+                        name='comment'
+                        type='text'
+                        placeholder='Deja aquí tu comentario'
+                        value={comment}
+                        onChange={onChangeValue}
+                    />
+                </Form>
+            </Segment>
+            <Button content='Guardar' onClick={onClickHandle} />
+        </Fragment>
     )
 }
 
--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,43 +1,72 @@
-import React from 'react'
-import { Segment, Header, Grid, GridColumn } from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react'
+import { Segment, Header, Grid, GridColumn, Label } from 'semantic-ui-react'
+import { db } from '../config/firebase'
+import '../css/CommentList.css'
 
 const CommentsList = (props) => {
     //console.log('props', props)
 
+    const [comments, setComments] = useState([])
+    const [obtener, setObtener] = useState(true)
+
+
+    //console.log('resultado', comments)
+
+    useEffect(
+        () => {
+            db.collection('pizzas').doc(props.idPizza).collection('comments').get().then(
+                res => {
+                    //console.log('comment docs', res.docs);
+                    const elements = res.docs.map(
+                        i => {
+                            //console.log('comments', i.data())
+                            return i.data()
+                        }
+                    );
+                    //console.log('comments', elements)
+                    setComments(elements)
+                    setObtener(false)
+                }
+            )
+
+        }, [obtener, props.idPizza]
+    )
+
     return (
         <React.Fragment>
             <Header as='h2'>Comentarios</Header>
-            <Segment.Group raised>
-            
-            {
-                props.comments.map(
+            <Segment raised className='comentarios' >
+
+                {comments.map(
                     (item, index) =>
-                        <Segment key={index}>
-                            <Grid  >
-                                <Grid.Row columns={10}>
-                                    <Grid.Column floated='left'>
-                                        <p>{item.user}</p>
+                        <div key={index} className='lista-comentarios'>
+                            <Grid >
+                                <Grid.Row columns={3}>
+                                    <Grid.Column >
+                                        <Label>user:</Label>  {item.user}
                                     </Grid.Column>
-                                    <Grid.Column floated='right'>
-                                        <p>{item.score}</p>
+                                    <Grid.Column>
+                                    </Grid.Column>
+                                    <Grid.Column textAlign='right'>
+                                        <Label>Puntuación:</Label> {item.score}
                                     </Grid.Column>
                                 </Grid.Row>
                                 <Grid.Row>
-                                    <GridColumn>
-                                        <p>{item.date}</p>
+                                    <GridColumn >
+                                        <Label  >Fecha:</Label> {item.date}
                                     </GridColumn>
                                 </Grid.Row>
-                                <Grid.Row>
+                                <Grid.Row className='comentario'>
                                     <GridColumn>
-                                        <p>{item.comment}</p>
+                                        <Header as='h4'>Comentario:</Header>
+                                        <Segment attached color='teal'>{item.comment}</Segment>
                                     </GridColumn>
                                 </Grid.Row>
                             </Grid>
-                        </Segment>
+                        </div>
+                )}
 
-                )
-            }
-        </Segment.Group>
+            </Segment>
         </React.Fragment>
     )
 }
