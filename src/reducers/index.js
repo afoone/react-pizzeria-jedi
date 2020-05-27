cfb710ecdf831044bb556f7842d7df9075985a74
jonadepr <jonadepr@gmail.com>
2020-05-27 09:35:54 +0200

    modificación redux no consulta db al borrar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,27 @@
-import { PIZZAS_FETCH_SUCCEED } from '../actions'
+import { PIZZAS_FETCH_SUCCEED, DELETE_PIZZA } from '../actions'
 import { global } from '../reduxInitiaState'
 
 const pizzas = (state = global, action) => {
     console.log("redux", action)
 
     switch (action.type) {
-    case PIZZAS_FETCH_SUCCEED:
-        return {
-            ...state,
-            pizzas: action.payload.pizzas
-        }
-      
-    
-    default:
-        break;
+
+        case PIZZAS_FETCH_SUCCEED:
+            return {
+                ...state,
+                pizzas: action.payload.pizzas
+            }
+
+        case DELETE_PIZZA:
+            return {
+                ...state,
+                pizzas: state.pizzas.filter(i => i.id !== action.payload.id),
+            }
+
+        default:
+            break;
     }
-    
+
     return state
 }
 
