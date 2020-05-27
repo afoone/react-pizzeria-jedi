f79688914cace4d6c451fca1026b9a89458ff7e6
jonadepr <jonadepr@gmail.com>
2020-05-27 08:59:20 +0200

    insertada promoPage en búsqueda errónea

--- src/components/PizzaSearch.jsx
+++ src/components/PizzaSearch.jsx
@@ -4,7 +4,7 @@ import { FiltroContext } from "../context/FiltroProvider";
 import { Card, Image, Message, Icon } from "semantic-ui-react";
 import { Redirect } from "react-router-dom";
 import '../css/PizzaSearch.css'
-
+import PromoPage from "../pages/PromoPage"
 const PizzaSearch = () => {
   const {
     pizzasFiltradas,
@@ -16,72 +16,77 @@ const PizzaSearch = () => {
   console.log("ingredientes buscados", ingredientes_Pizzas);
   console.log("search", pizzasFiltradas);
   console.log("buscarPizzas", buscarPizzas);
- 
+
 
   return (
-   
+
     <div className='pizza-search' >
-    { buscarPizzas ? null : <Redirect to='/'></Redirect> }
-    { ingredientes_Pizzas.length > 0 || pizzasFiltradas.length > 0 ? null :
-      (<Message negative>
-       <Message.Header>
-         Lo sentimos mucho no hay resultados de su busqueda<Icon name="frown outline" size="large" />
-       </Message.Header>
-       <p><strong>Pruebe con otra busqueda o mire nuestras Pizzas y Promociones</strong></p>
-     </Message>) 
-   }
-    <>
-      {pizzasFiltradas.map((item, index) => (
-        <>
-        <div>
-          <Card className='tarjeta-pizza'>
+      {buscarPizzas ? null : <Redirect to='/'></Redirect>}
+      {ingredientes_Pizzas.length > 0 || pizzasFiltradas.length > 0 ? null :
+        (
+          <>
+            <Message negative>
+              <Message.Header>
+                Lo sentimos mucho no hay resultados de su busqueda<Icon name="frown outline" size="large" />
+              </Message.Header>
+              <p><strong>Pruebe con otra busqueda o mire nuestras Pizzas y Promociones</strong></p>
+            </Message>
+            <PromoPage />
+          </>
+        )
+      }
+      <>
+        {pizzasFiltradas.map((item, index) => (
+          <>
+            <div>
+              <Card className='tarjeta-pizza'>
+                <Image src={item.image} wrapped ui={false} />
+                <Card.Content >
+                  <Card.Header >{item.name}</Card.Header>
+                  <Card.Meta>{item.price} €</Card.Meta>
+                  {
+                    item.novelty === true ? <Card.Description>Novedad!! </Card.Description> :
+                      <Card.Description>Prueba nuestras pizzas clasicas</Card.Description>
+                  }
+
+                </Card.Content>
+                <Card.Content extra>
+                  <>
+                    <ModalImage image={item.image} name={item.name} price={item.price} id={item.id}></ModalImage>
+
+                  </>
+                </Card.Content>
+              </Card>
+            </div>
+          </>
+        ))}
+        {ingredientes_Pizzas.map((item, index) => (
+          <Card key={index}>
             <Image src={item.image} wrapped ui={false} />
-            <Card.Content >
-              <Card.Header >{item.name}</Card.Header>
+            <Card.Content>
+              <Card.Header>{item.name}</Card.Header>
               <Card.Meta>{item.price} €</Card.Meta>
-              {
-                item.novelty === true ?  <Card.Description>Novedad!! </Card.Description> :
-                <Card.Description>Prueba nuestras pizzas clasicas</Card.Description>
-              }
-              
+              {item.novelty === true ? (
+                <Card.Description>Novedad!! </Card.Description>
+              ) : (
+                  <Card.Description>
+                    Prueba nuestras pizzas clasicas
+                  </Card.Description>
+                )}
             </Card.Content>
             <Card.Content extra>
-              <>
-                <ModalImage image={item.image} name={item.name} price={item.price} id={item.id}></ModalImage>
-                
-              </>
+              <a>
+                <ModalImage
+                  image={item.image}
+                  name={item.name}
+                  price={item.price}
+                  id={item.id}
+                ></ModalImage>
+                <Icon name="zoom-in" size="large" />
+              </a>
             </Card.Content>
           </Card>
-          </div>
-        </>
-      ))}
-      {ingredientes_Pizzas.map((item, index) => (
-        <Card key={index}>
-          <Image src={item.image} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{item.name}</Card.Header>
-            <Card.Meta>{item.price} €</Card.Meta>
-            {item.novelty === true ? (
-              <Card.Description>Novedad!! </Card.Description>
-            ) : (
-              <Card.Description>
-                Prueba nuestras pizzas clasicas
-              </Card.Description>
-            )}
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <ModalImage
-                image={item.image}
-                name={item.name}
-                price={item.price}
-                id={item.id}
-              ></ModalImage>
-              <Icon name="zoom-in" size="large" />
-            </a>
-          </Card.Content>
-        </Card>
-      ))}
+        ))}
       </>
     </div>
   );
